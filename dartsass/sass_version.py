75ce0c68e8458b60d7783c00cad2d295aeb8cfc0
David THENON <sveetch@gmail.com>
2023-09-11 12:28:09 +0000

    [CHG] Updated proto python module with a new version compiled with the right protoc version (libprotoc 24.2)

--- dartsass/sass_version.py
+++ dartsass/sass_version.py
@@ -1,4 +1,4 @@
 from dartsass.exceptions import AppOperationError
 from dartsass.embedded_sass_pb2 import InboundMessage, OutboundMessage
 
-print("Currently failing because of proto module compiled without incompatible protoc version for protobuf package.")
+print("Hello world!")
